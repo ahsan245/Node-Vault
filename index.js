adabceb7332f5e6953ec98c1b2f3f3129f6cc860
ahsan245 <muhammadahsanty@gmail.com>
2023-10-17 09:17:39 -0700

    Done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
 app.get('/', (req, res) => {
-  res.send('Hello From NoteVault!');
+  res.send('Hello From NoteVat!');
 })
 
 app.listen(process.env.port,function(){
